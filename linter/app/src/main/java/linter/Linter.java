a3d76519efc089b20177abc5c86497c3eb29f5b1
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-05-18 22:43:52 -0700

    started working on FILE IO project.

---
+++ linter/app/src/main/java/linter/Linter.java
@@ -0,0 +1,11 @@
+package linter;
+
+import java.io.File;
+
+public class Linter {
+    public static String lintThisFile(String filepath) {
+        File userFile = new File(filepath);
+
+        return "";
+    }
+}
