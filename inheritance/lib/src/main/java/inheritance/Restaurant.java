9a947b6832fbf50c87d1239bf3c62f0e4cb60015
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-05-23 20:51:20 -0700

    experimenting with reviews as collection in restaurant.

--- inheritance/lib/src/main/java/inheritance/Restaurant.java
+++ inheritance/lib/src/main/java/inheritance/Restaurant.java
@@ -1,13 +1,17 @@
 package inheritance;
 
+import java.util.ArrayList;
+
 public class Restaurant {
     String name;
     int stars; // 1-5 rating
     int price; // 1-5 dollar signs
+    ArrayList<Review> reviews;
     public Restaurant(String name) {
         this.name = name;
         stars = 0;
         price = 0;
+        reviews = new ArrayList<>();
     }
 
     @Override
